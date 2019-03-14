ab49f985707b2fe5196b284c6d3328f6e390f9c4
dzarling1 <43392154+dzarling1@users.noreply.github.com>
2019-03-14 12:23:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 var page_load = function() {
-  var xmlhttp = new XMLHttpRequest();
-  var url = "https://dzarling1.github.io/index.json";
+    console.log("Page loaded");
+    var xmlhttp = new XMLHttpRequest();
+    var url = "https://dzarling1.github.io/index.json";
 
-  xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-          var myArr = JSON.parse(this.responseText);
-          myFunction(myArr);
-      }
-  };
-  xmlhttp.open("GET", url, true);
-  xmlhttp.send();
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("JSON loaded");
+            var myArr = JSON.parse(this.responseText);
+            myFunction(myArr);
+        }
+    };
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
 
-  function myFunction(arr) {
-      var out = "";
-      var i;
-      for(i = 0; i < arr.length; i++) {
-          out += '<a href="' + arr[i].url + '">' +
-          arr[i].display + '</a><br>';
-      }
-      document.getElementById("json").innerHTML = out;
-  }
+    function myFunction(arr) {
+        var out = "";
+        var i;
+        for(i = 0; i < arr.length; i++) {
+            out += '<a href="' + arr[i].url + '">' +
+            arr[i].display + '</a><br>';
+        }
+        document.getElementById("json").innerHTML = out;
+    }
 }
