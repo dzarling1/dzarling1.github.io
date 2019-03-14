fb694eb8f25a889941b79bb4e3601c7c37adfb32
dzarling1 <43392154+dzarling1@users.noreply.github.com>
2019-03-14 11:50:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
 var page_load = function() {
-    console.log("hello world");
-    alert("Page loaded");
-    var x = 10;
-    var y = 5;
-    var z = 1;
-    var avg = (x + y + z)/3;
-    console.log(avg);
-    if(avg == 5+1/3) {
-        console.log("equal");
-    }
-    var zero = 0;
-    var boolean_false = false;
-    if(zero === boolean_false) {
-        console.log("error");
-    }
-    else{
-        console.log("fine");
-    }
-    var p_new = document.createElement("p");
-    p_new.textContent = "inserted a new paragraph!";
-    document.body.appendChild(p_new);
-    //p_new onclick="console.log(\"clicked on p1\")";
-    
-    class Math {
-        constructor(i, j) {
-            this.x = i;
-            this.y = j;
-            this.sum = i + j;
-        }
-        print() {
-            console.log(this.x, this.y, this.sum);
-        }
-    }
-    var XY = new Math(1, 3);
-    XY.print()
+    var json = JSON.parse(index)
+    console.log(json);
 }
