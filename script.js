138a238b920cd11c7da51ae5cf6976435aaa29fe
dzarling1 <43392154+dzarling1@users.noreply.github.com>
2019-03-14 13:37:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,13 +21,15 @@ var page_load = function() {
         for(i = 0; i < arr.Completed_Courses.length-1; i++) {
             out += arr.Completed_Courses[i].name+", ";
         }
-        out += "and " + arr.Completed_Courses[i].name+"\n";
-        out += "Current classes: ";
+        out += "and " + arr.Completed_Courses[i].name;
+        var out2 += "Current classes: ";
         for(i = 0; i < arr.Current_Courses.length-1; i++) {
-            out += arr.Current_Courses[i].name+", ";
+            out2 += arr.Current_Courses[i].name+", ";
         }
-        out += "and " + arr.Current_Courses[i].name+"\n";
+        out2 += "and " + arr.Current_Courses[i].name+"\n";
         console.log(out);
-        document.getElementById("json").innerHTML = out;
+        console.log(out2);
+        document.getElementById("json1").innerHTML = out;
+        document.getElementById("json2").innerHTML = out2;
     }
 }
